0e9104cbc3dbfbeab3040574d9623cb6a35b02d9
tluolamo <tluolamo@gmail.com>
2020-06-15 16:54:36 -0700

    simple json api

--- myApp.js
+++ myApp.js
@@ -23,6 +23,9 @@ app.get('/', (req, res) => {
 app.use(express.static(__dirname + '/public'))
 
 /** 5) serve JSON on a specific route */
+app.get('/json', (req, res) => {
+  res.json({ message: 'Hello json' })
+})
 
 /** 6) Use the .env file to configure the app */
 
